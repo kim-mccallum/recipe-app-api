70a97a50f3ec1b75e45673e18f6a27f15dd55512
Kim McCallum <kchaase@gmail.com>
2021-01-09 17:07:58 -0700

    added users routes including signup and login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const recipesRoutes = require("./routes/recipes-routes");
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/recipes", recipesRoutes);
+app.use("/api/users", usersRoutes);
 
 //middleware to catch requests to unsupported routes
 app.use((req, res, next) => {
